d2472a7e3cb2c86bd401aedae6ac994ade083d59
tanishq14developer <tanishq.happy@gmail.com>
2023-02-02 02:40:24 +0530

    Feat:Get Started fragment added

--- app/src/main/java/com/zingit/restaurant/MainActivity.kt
+++ app/src/main/java/com/zingit/restaurant/MainActivity.kt
@@ -2,10 +2,19 @@ package com.zingit.restaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.zingit.restaurant.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
--- app/src/main/java/com/zingit/restaurant/views/SplashActivity.kt
+++ app/src/main/java/com/zingit/restaurant/views/SplashActivity.kt
@@ -1,12 +1,22 @@
 package com.zingit.restaurant.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.zingit.restaurant.MainActivity
 import com.zingit.restaurant.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        lifecycleScope.launch {
+            delay(2000)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
     }
 }
