bd0b9bad357850a167d04e7079a15e691bcac149
tanishq14developer <tanishq.happy@gmail.com>
2023-02-02 22:09:54 +0530

    Feat:login_signUp screen developed

---
+++ app/src/main/java/com/zingit/restaurant/views/signUp/LoginSignUpFragment.kt
@@ -0,0 +1,27 @@
+package com.zingit.restaurant.views.signUp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.zingit.restaurant.R
+
+class LoginSignUpFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_sign_up, container, false)
+    }
+
+
+}
