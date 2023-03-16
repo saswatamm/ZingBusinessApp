4dbef295a7d9e531d02e550eb6e1396ab19d7eae
tanishq14developer <tanishq.happy@gmail.com>
2023-03-16 23:58:23 +0530

    Feat:added printer

--- app/src/main/java/com/zingit/restaurant/views/home/HomeFragment.kt
+++ app/src/main/java/com/zingit/restaurant/views/home/HomeFragment.kt
@@ -1,22 +1,36 @@
 package com.zingit.restaurant.views.home
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.DisplayMetrics
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.dantsu.escposprinter.connection.DeviceConnection
+import com.dantsu.escposprinter.connection.bluetooth.BluetoothConnection
+import com.dantsu.escposprinter.textparser.PrinterTextParserImg
 import com.google.firebase.firestore.*
+import com.google.firebase.firestore.EventListener
 import com.zingit.restaurant.R
 import com.zingit.restaurant.databinding.FragmentHomeBinding
 import com.zingit.restaurant.utils.hideKeyboard
+import com.zingit.restaurant.utils.printer.AsyncBluetoothEscPosPrint
+import com.zingit.restaurant.utils.printer.AsyncEscPosPrint
+import com.zingit.restaurant.utils.printer.AsyncEscPosPrinter
 import com.zingit.restaurant.views.FirebaseNotificationActionActivity
-import javax.inject.Inject
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class HomeFragment : Fragment() {
@@ -25,8 +39,14 @@ class HomeFragment : Fragment() {
     lateinit var firestore: FirebaseFirestore
     private val TAG = "HomeFragment"
     lateinit var query: Query
+    private val selectedDevice: BluetoothConnection? = null
+    val PERMISSION_BLUETOOTH = 1
+    val PERMISSION_BLUETOOTH_ADMIN = 2
+    val PERMISSION_BLUETOOTH_CONNECT = 3
+    val PERMISSION_BLUETOOTH_SCAN = 4
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkPermissions()
 
     }
 
@@ -52,6 +72,9 @@ class HomeFragment : Fragment() {
                             DocumentChange.Type.ADDED -> {
                                 Toast.makeText(requireContext(), i.document.data.get("paymentOrderID").toString(), Toast.LENGTH_SHORT).show()
                                 Log.e(TAG, "onEvent: ${i.document.data}")
+                                printBluetooth()
+
+
 
                                 /*
 
@@ -123,6 +146,151 @@ class HomeFragment : Fragment() {
 
 
         return binding.root
+    }
+
+    fun printBluetooth() {
+        if (ContextCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.BLUETOOTH
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.BLUETOOTH),
+                PERMISSION_BLUETOOTH
+            )
+        } else if (ContextCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.BLUETOOTH_ADMIN
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.BLUETOOTH_ADMIN),
+                HomeFragment().PERMISSION_BLUETOOTH_ADMIN
+            )
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && ContextCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.BLUETOOTH_CONNECT
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.BLUETOOTH_CONNECT),
+                HomeFragment().PERMISSION_BLUETOOTH_CONNECT
+            )
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && ContextCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.BLUETOOTH_SCAN
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.BLUETOOTH_SCAN),
+                HomeFragment().PERMISSION_BLUETOOTH_SCAN
+            )
+        } else {
+            AsyncBluetoothEscPosPrint(
+                requireActivity(),
+                object : AsyncEscPosPrint.OnPrintFinished() {
+                   override fun onError(asyncEscPosPrinter: AsyncEscPosPrinter?, codeException: Int) {
+                        Log.e(
+                            "Async.OnPrintFinished",
+                            "AsyncEscPosPrint.OnPrintFinished : An error occurred !"
+                        )
+                    }
+
+                   override fun onSuccess(asyncEscPosPrinter: AsyncEscPosPrinter?) {
+                        Log.i(
+                            "Async.OnPrintFinished",
+                            "AsyncEscPosPrint.OnPrintFinished : Print is finished !"
+                        )
+
+                    }
+                }
+            )
+                .execute(this.getAsyncEscPosPrinter(selectedDevice))
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            when (requestCode) {
+                HomeFragment().PERMISSION_BLUETOOTH, HomeFragment().PERMISSION_BLUETOOTH_ADMIN, HomeFragment().PERMISSION_BLUETOOTH_CONNECT, HomeFragment().PERMISSION_BLUETOOTH_SCAN -> printBluetooth()
+            }
+        }
+    }
+
+    private fun checkPermissions() {
+        val permission1 =
+            ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        val permission2 =
+            ActivityCompat.checkSelfPermission(requireActivity(), Manifest.permission.BLUETOOTH_SCAN)
+        if (permission1 != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                requireActivity(), arrayOf(Manifest.permission.BLUETOOTH_CONNECT),
+                1
+            )
+        } else if (permission2 != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                requireActivity(), arrayOf(Manifest.permission.BLUETOOTH_CONNECT),
+                1
+            )
+        }
+    }
+
+    fun getAsyncEscPosPrinter(printerConnection: DeviceConnection?): AsyncEscPosPrinter? {
+        val format = SimpleDateFormat("'on' yyyy-MM-dd 'at' HH:mm:ss")
+        val printer = AsyncEscPosPrinter(printerConnection, 203, 48f, 32)
+        return printer.addTextToPrint(
+            """
+            [C]<img>${
+                PrinterTextParserImg.bitmapToHexadecimalString(
+                    printer,
+                   requireActivity().getResources()
+                        .getDrawableForDensity(
+                            R.drawable.zing_business_logo,
+                            DisplayMetrics.DENSITY_MEDIUM
+                        )
+                )
+            }</img>
+            [L]
+            [C]<u><font size='big'>ORDER N°045</font></u>
+            [L]
+            [C]<u type='double'>${format.format(Date())}</u>
+            [C]
+            [C]================================
+            [L]
+            [L]<b>BEAUTIFUL SHIRT</b>[R]9.99€
+            [L]  + Size : S
+            [L]
+            [L]<b>AWESOME HAT</b>[R]24.99€
+            [L]  + Size : 57/58
+            [L]
+            [C]--------------------------------
+            [R]TOTAL PRICE :[R]34.98€
+            [R]TAX :[R]4.23€
+            [L]
+            [C]================================
+            [L]
+            [L]<u><font color='bg-black' size='tall'>Customer :</font></u>
+            [L]Raymond DUPONT
+            [L]5 rue des girafes
+            [L]31547 PERPETES
+            [L]Tel : +33801201456
+            
+            [C]<barcode type='ean13' height='10'>831254784551</barcode>
+            [L]
+            [C]<qrcode size='20'>http://www.developpeur-web.dantsu.com/</qrcode>
+            
+            """.trimIndent()
+        )
     }
 
 
