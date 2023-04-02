99e3dcecb68223bca96049b879fa716e92fb8b20
tanishq14developer <tanishq.happy@gmail.com>
2023-04-02 23:52:20 +0530

    Feat:setting functionality

--- app/src/main/java/com/zingit/restaurant/views/setting/SettingsFragment.kt
+++ app/src/main/java/com/zingit/restaurant/views/setting/SettingsFragment.kt
@@ -6,13 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.zingit.restaurant.R
+import com.zingit.restaurant.databinding.BottomSheetDeactivateBinding
+import com.zingit.restaurant.databinding.BottomSheetLogoutBinding
+import com.zingit.restaurant.databinding.BottomSheetPrinterBinding
 import com.zingit.restaurant.databinding.FragmentSettingsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 
 class SettingsFragment : Fragment() {
@@ -28,9 +30,89 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding =DataBindingUtil.inflate(inflater,R.layout.fragment_settings, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
+        binding.apply {
+            help.setOnClickListener {
+                findNavController().navigate(R.id.action_settingsFragment_to_helpFragment)
+            }
+            testPrinter.setOnClickListener {
+                testPrinter()
+            }
+            logout.setOnClickListener {
+                logout()
+            }
+            requestDeactivate.setOnClickListener {
+                deactivateAccount()
+            }
+        }
 
         return binding.root
+    }
+
+    fun testPrinter() {
+        val binding: BottomSheetPrinterBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(context),
+            R.layout.bottom_sheet_printer,
+            null,
+            false
+        )
+        val dialog = BottomSheetDialog(requireContext(), R.style.BottomSheetDialog)
+        dialog.setCancelable(false)
+        binding.apply {
+            cancel.setOnClickListener {
+                dialog.dismiss()
+            }
+            close.setOnClickListener {
+                dialog.dismiss()
+            }
+        }
+        dialog.setContentView(binding.root)
+        dialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        dialog.show()
+    }
+
+    fun logout() {
+        val binding: BottomSheetLogoutBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(context),
+            R.layout.bottom_sheet_logout,
+            null,
+            false
+        )
+        val dialog = BottomSheetDialog(requireContext(), R.style.BottomSheetDialog)
+        dialog.setCancelable(false)
+        binding.apply {
+            cancel.setOnClickListener {
+                dialog.dismiss()
+            }
+            close.setOnClickListener {
+                dialog.dismiss()
+            }
+        }
+        dialog.setContentView(binding.root)
+        dialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        dialog.show()
+    }
+
+    fun deactivateAccount() {
+        val binding: BottomSheetDeactivateBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(context),
+            R.layout.bottom_sheet_deactivate,
+            null,
+            false
+        )
+        val dialog = BottomSheetDialog(requireContext(), R.style.BottomSheetDialog)
+        dialog.setCancelable(false)
+        binding.apply {
+            cancel.setOnClickListener {
+                dialog.dismiss()
+            }
+            close.setOnClickListener {
+                dialog.dismiss()
+            }
+        }
+        dialog.setContentView(binding.root)
+        dialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        dialog.show()
     }
 
 
