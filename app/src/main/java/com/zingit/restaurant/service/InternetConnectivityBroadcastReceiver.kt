fd0863ff34b5f7f168932c1801bc471500cae801
tanishq14developer <tanishq.happy@gmail.com>
2023-04-05 21:46:28 +0530

    Feat:updated and bugs fixed

--- app/src/main/java/com/zingit/restaurant/service/InternetConnectivityBroadcastReceiver.kt
+++ app/src/main/java/com/zingit/restaurant/service/InternetConnectivityBroadcastReceiver.kt
@@ -1,18 +1,50 @@
 package com.zingit.restaurant.service
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import com.zingit.restaurant.utils.NetworkUtil
 
-class InternetConnectivityBroadcastReceiver(private val isInternetConnected :MutableLiveData<Boolean>):BroadcastReceiver() {
+class InternetConnectivityBroadcastReceiver(
+    private val isInternetConnected: MutableLiveData<Boolean>,
+    private val isBluetoothConnected: MutableLiveData<Boolean>
+) :
+    BroadcastReceiver() {
+    private  val TAG = "BroadcastReciever"
     override fun onReceive(p0: Context?, p1: Intent?) {
         val status = NetworkUtil.getConnectivityStatusString(p0!!)
-        if(status){
+        if (status) {
             isInternetConnected.postValue(true)
-        }else{
+        } else {
             isInternetConnected.postValue(false)
         }
+
+        val action = p1?.action
+        if (isBluetoothAvailable()) {
+            Log.e(TAG, "onReceive: ${isBluetoothAvailable()}")
+            isBluetoothConnected.value = true
+        } else {
+            isBluetoothConnected.value = false
+        }
+        if (BluetoothDevice.ACTION_ACL_CONNECTED == action) {
+            isBluetoothConnected.value = true
+
+        } else if (BluetoothDevice.ACTION_ACL_DISCONNECTED == action) {
+            // Bluetooth device is disconnected
+            isBluetoothConnected.value = false
+        }
+
+
+    }
+
+    fun isBluetoothAvailable(): Boolean {
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        return bluetoothAdapter != null && bluetoothAdapter.isEnabled && bluetoothAdapter.state == BluetoothAdapter.STATE_ON
+
     }
 }
--- app/src/main/java/com/zingit/restaurant/views/RootActivity.kt
+++ app/src/main/java/com/zingit/restaurant/views/RootActivity.kt
@@ -13,48 +13,57 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.zingit.restaurant.R
 import com.zingit.restaurant.databinding.ActivityHomeMainBinding
-import com.zingit.restaurant.service.BluetoothBroadcastReceiver
 import com.zingit.restaurant.service.InternetConnectivityBroadcastReceiver
 
 class RootActivity : AppCompatActivity() {
     lateinit var binding: ActivityHomeMainBinding
     lateinit var navController: NavController
     lateinit var internetConnectivityReceiver: InternetConnectivityBroadcastReceiver
-    lateinit var isBluetoothBroadcastReceiver: BluetoothBroadcastReceiver
     private val TAG = "RootActivity"
     private val isInternetConnectivity = MutableLiveData<Boolean>()
     private val isBluetoothConnected = MutableLiveData<Boolean>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home_main)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHostFragment.navController
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
-        isInternetConnectivity.observe(this@RootActivity) {
-            if (it) {
-                binding.fragmentContainerView.visibility = View.VISIBLE
-                binding.noInternet.root.visibility = View.GONE
-            } else {
-                binding.fragmentContainerView.visibility = View.GONE
-                binding.noInternet.root.visibility = View.VISIBLE
-                binding.noInternet.getStarted.setOnClickListener {
-                    startActivity(Intent(Settings.ACTION_WIFI_SETTINGS))
+        isInternetConnectivity.observe(this@RootActivity) { inter ->
+            isBluetoothConnected.observe(this@RootActivity) { blue ->
+
+                if (inter && blue) {
+                    binding.fragmentContainerView.visibility = View.VISIBLE
+                    binding.noInternet.root.visibility = View.GONE
+                    binding.noBluetooth.root.visibility = View.GONE
+                } else if (inter && !blue) {
+                    binding.fragmentContainerView.visibility = View.GONE
+                    binding.noInternet.root.visibility = View.GONE
+                    binding.noBluetooth.root.visibility = View.VISIBLE
+                    binding.noBluetooth.getStarted.setOnClickListener {
+                        startActivity(Intent(Settings.ACTION_BLUETOOTH_SETTINGS))
+                    }
+                } else if (!inter && blue) {
+                    binding.fragmentContainerView.visibility = View.GONE
+                    binding.noInternet.root.visibility = View.VISIBLE
+                    binding.noBluetooth.root.visibility = View.GONE
+                    binding.noInternet.getStarted.setOnClickListener {
+                        startActivity(Intent(Settings.ACTION_WIFI_SETTINGS))
+                    }
+                } else {
+//                    binding.fragmentContainerView.visibility = View.GONE
+//                    binding.noInternet.root.visibility = View.VISIBLE
+//                    binding.noBluetooth.root.visibility = View.VISIBLE
+//                    binding.noInternet.getStarted.setOnClickListener {
+//                        startActivity(Intent(Settings.ACTION_WIFI_SETTINGS))
+//                    }
+//                    binding.noBluetooth.getStarted.setOnClickListener {
+//                        startActivity(Intent(Settings.ACTION_BLUETOOTH_SETTINGS))
+//                    }
                 }
+
             }
 
-        }
-        isBluetoothConnected.observe(this@RootActivity) {
-            if (it) {
-                binding.fragmentContainerView.visibility = View.VISIBLE
-                binding.noBluetooth.root.visibility = View.GONE
-            } else {
-                binding.fragmentContainerView.visibility = View.GONE
-                binding.noBluetooth.root.visibility = View.VISIBLE
-                binding.noBluetooth.getStarted.setOnClickListener{
-                    startActivity(Intent(Settings.ACTION_BLUETOOTH_SETTINGS))
-                }
-
-            }
         }
 
 
@@ -64,25 +73,21 @@ class RootActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         unregisterReceiver(internetConnectivityReceiver)
-        unregisterReceiver(isBluetoothBroadcastReceiver)
     }
-
-
 
 
     override fun onStart() {
         super.onStart()
-        isBluetoothBroadcastReceiver = BluetoothBroadcastReceiver(isBluetoothConnected)
-        internetConnectivityReceiver = InternetConnectivityBroadcastReceiver(isInternetConnectivity)
+        internetConnectivityReceiver =
+            InternetConnectivityBroadcastReceiver(isInternetConnectivity, isBluetoothConnected)
         registerReceiver(internetConnectivityReceiver, IntentFilter().apply {
             addAction("android.net.conn.CONNECTIVITY_CHANGE")
-        })
-
-        registerReceiver(isBluetoothBroadcastReceiver, IntentFilter().apply {
             addAction("android.bluetooth.device.action.ACL_CONNECTED")
             addAction("android.bluetooth.device.action.ACL_DISCONNECTED")
             addAction("android.bluetooth.adapter.action.STATE_CHANGED")
         })
+
+
     }
 
 
