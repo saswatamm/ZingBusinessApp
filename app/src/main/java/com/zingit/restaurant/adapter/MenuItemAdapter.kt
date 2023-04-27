141b6babe8823291ee74ff637654e6a60f0051dd
tanishq14developer <tanishq.happy@gmail.com>
2023-04-28 00:40:08 +0530

    Feat:implemented menu available or not

--- app/src/main/java/com/zingit/restaurant/adapter/MenuItemAdapter.kt
+++ app/src/main/java/com/zingit/restaurant/adapter/MenuItemAdapter.kt
@@ -6,14 +6,17 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
 import com.zingit.restaurant.databinding.SingleItemMenuOptionBinding
 
 import com.zingit.restaurant.models.item.ItemMenuModel
+import com.zingit.restaurant.utils.Utils
 
 
 class MenuItemAdapter(private val context: Context) : ListAdapter<ItemMenuModel, MenuItemAdapter.MenuViewHolder>(MenuDiffUtils()) {
 
     private  val TAG = "MenuItemAdapter"
+    lateinit var firestore: FirebaseFirestore
 
     inner class MenuViewHolder(val binding: SingleItemMenuOptionBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -34,8 +37,18 @@ class MenuItemAdapter(private val context: Context) : ListAdapter<ItemMenuModel,
     }
 
     override fun onBindViewHolder(holder: MenuItemAdapter.MenuViewHolder, position: Int) {
+        firestore = FirebaseFirestore.getInstance()
         val itemModel =getItem(position)
         holder.bind(itemModel)
+        holder.binding.apply {
+            switchToggle.setOnCheckedChangeListener{ view, isChecked ->
+                if(isChecked){
+                    firestore.collection("item").document(getItem(position).id).update("availableOrNot",true)
+                }else{
+                    firestore.collection("item").document(getItem(position).id).update("availableOrNot",false)
+                }
+            }
+        }
     }
 
 }
